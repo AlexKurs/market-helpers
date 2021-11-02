2cfb28d6bbf77a67c56e639c5ab776956d972308
AlexKurs <kurshalow@gmail.com>
2021-11-02 09:15:42 +0100

    Upload ready scripts

---
+++ Fibonacci.py
@@ -0,0 +1,58 @@
+# Fibonacci Levels
+
+h = 1802.90
+l = 1772.40
+
+d = h-l
+
+# Gerade Rechnung
+
+gf0 = round(d * 0 + l, 2)
+gf23 = round(d * 0.236 + l, 2)
+gf38 = round(d * 0.382 + l, 2)
+gf50 = round(d * 0.50 + l, 2)
+gf61 = round(d * 0.618 + l, 2)
+gf100 = round(d * 1 + l, 2)
+gf138 = round(d * 1.382 + l, 2)
+gf161 = round(d * 1.618 + l, 2)
+gf200 = round(d * 2 + l, 2)
+gf261 = round(d * 2.618 + l, 2)
+
+# Zurück Rechnung
+
+zf0 = round(h - (d * 0), 2)
+zf23 = round(h - (d * 0.236), 2)
+zf38 = round(h - (d * 0.382), 2)
+zf50 = round(h - (d * 0.50), 2)
+zf61 = round(h - (d * 0.618), 2)
+zf100 = round(h - (d * 1), 2)
+zf138 = round(h - (d * 1.382), 2)
+zf161 = round(h - (d * 1.618), 2)
+zf200 = round(h - (d * 2), 2)
+zf261 = round(h - (d * 2.618), 2)
+
+
+
+print('%.2f' % d)
+print('------------')
+print('%.2f' % gf0)
+print('%.2f' % gf23)
+print('%.2f' % gf38)
+print('%.2f' % gf50)
+print('%.2f' % gf61)
+print('%.2f' % gf100)
+print('%.2f' % gf161)
+print('%.2f' % gf200)
+print('%.2f' % gf261)
+print('------------')
+print('%.2f' % zf0)
+print('%.2f' % zf23)
+print('%.2f' % zf38)
+print('%.2f' % zf50)
+print('%.2f' % zf61)
+print('%.2f' % zf61)
+print('%.2f' % zf100)
+print('%.2f' % zf138)
+print('%.2f' % zf161)
+print('%.2f' % zf200)
+print('%.2f' % zf261)
---
+++ Levels.py
@@ -0,0 +1,95 @@
+# Klassik Methode
+# Pivot Point
+
+o = 1801.50
+h = 1802.90
+l = 1772.40
+c = 1783.90
+
+kp = (h+l+c)/3
+
+# Widerstand
+
+kr1 = ((2*kp)-l)
+kr2 = kp+h-l
+kr3 = h+2*(kp-l)
+
+# Unterstützung
+
+ks1 = (2*kp)-h
+ks2 = kp-h+l
+ks3 = l-2*(h-kp)
+
+# Woody Methode
+# Pivot Point
+
+wp = (h+l+2*c)/4
+
+# Widerstand
+
+wr1 = 2*wp-l
+wr2 = wp+h-l
+
+# Unterstützung
+
+ws1 = 2*wp-h
+ws2 = wp-h+l
+
+# Camarilla Methode
+# Unterstützung
+
+cr1 = (h-l)*1.1/12+c
+cr2 = (h-l)*1.1/6+c
+cr3 = (h-l)*1.1/4+c
+cr4 = (h-l)*1.1/2+c
+
+# Widerstand
+
+cs1 = c-(h-l)*1.1/12
+cs2 = c-(h-l)*1.1/6
+cs3 = c-(h-l)*1.1/4
+cs4 = c-(h-l)*1.1/2
+
+# DeMark Methode
+
+if c < o:
+    x = h+2*l+c
+elif c > o:
+    x = 2*h+l+c
+elif c == o:
+    x = h+l+2*c
+
+nh = x/2-l
+nl = x/2-h
+
+# Durchschnittliche Werte
+
+p = (kp+wp)/2
+
+r1 = (kr1+wr1+cr1)/3
+r2 = (kr2+wr2+cr2)/3
+r3 = (kr3+cr3)/2
+r4 = cr4
+
+s1 = (ks1+ws1+cs1)/3
+s2 = (ks2+ws2+cs2)/3
+s3 = (ks3+cs3)/2
+s4  = cs4
+
+print('Pivot:', '%.2f' % p)
+print('------------------')
+
+print('R1:', '%.2f' % r1)
+print('R2:', '%.2f' % r2)
+print('R3:', '%.2f' % r3)
+print('R4:', '%.2f' % r3)
+print('------------------')
+print('S1:', '%.2f' % s1)
+print('S2:', '%.2f' % s2)
+print('S3:', '%.2f' % s3)
+print('S4:', '%.2f' % s3)
+print('------------------')
+print('DeMark Methode - Max & Min')
+print('------------------')
+print('New High:', '%.2f' % nh)
+print('New Low:', '%.2f' % nl)
